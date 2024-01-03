3d480a12ff6ce7308b79ecf42f2b27cb04f77e88
Suraj Gojanur <surajgojanur@gmail.com>
2024-01-03 22:47:22 +0530

    working

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
 const express = require('express');
-const app = express();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv");
 
 dotenv.config();
+const apiKey = process.env.API_KEY;
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(apiKey);
+const app = express();
 
-// Middleware to parse JSON in request body
 app.use(express.json());
+app.use(express.static('public'));
 
 app.post('/generateText', async (req, res) => {
-    const input = req.body.input; // Assuming the input comes from the POST request
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const result = await model.generateContentStream([input]);
-    let generatedText = '';
+    try {
+        const input = req.body.input; // Assuming the input comes from the POST request
 
-    for await (const chunk of result.stream) {
-        const chunkText = chunk.text();
-        generatedText += chunkText;
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+        const result = await model.generateContentStream([input]);
+        let generatedText = '';
+
+        for await (const chunk of result.stream) {
+            const chunkText = chunk.text();
+            generatedText += chunkText;
+        }
+
+        res.send(generatedText); // Send the generated text as the response
+    } catch (error) {
+        console.error('Error generating text:', error);
+        res.status(500).send('Error generating text. Please check the server logs.');
     }
-
-    res.send(generatedText); // Send the generated text as the response
 });
 
 app.listen(3000, () => {
